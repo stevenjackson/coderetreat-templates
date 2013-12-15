91cdc572c684c2f6c2e8b2c82827809c6a22edbb
Steve Jackson <steve.jackson@leandogsoftware.com>
2013-12-15 12:37:34 -0500

    updated for ruby, javascript, and java

--- javascript/test/test.js
+++ javascript/test/test.js
@@ -1,10 +1,8 @@
 // https://github.com/LearnBoost/expect.js/blob/master/README.md
-var expect = require("expect.js")
-
 describe('Game of Life', function(){
 	describe('Baseline', function(){
 		it('should fail miserably', function(){
-			expect(false).to.be.ok();
+			expect(test()).toBeFalsy();
 		});
 	});
 });
